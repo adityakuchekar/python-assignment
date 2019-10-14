ca68a95a30d1f6ced1b7e9881d4cb8e2cee6a760
adityakuchekar <adityakuchekar@gmail.com>
2019-10-14 15:39:55 +0530

    update pymongo file

--- pymongo_database.py
+++ pymongo_database.py
@@ -2,6 +2,6 @@ def func_Q1(database_object):
     distinct_students = database_object["students"].distinct("_id")
     return len(list(distinct_students))
 
-def func_Q1(database_object):
+def func_Q2(database_object):
     distinct_students = database_object["grades"].distinct("class_id")
     return len(list(distinct_students))
